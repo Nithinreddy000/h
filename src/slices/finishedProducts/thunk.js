d563a2061a0afd77b6ab343ca88c927f624bd063
Reddy Nithin Reddy <129539615+Nithinreddy000@users.noreply.github.com>
2024-07-08 00:23:27 +0530

    Update thunk.js

--- src/slices/finishedProducts/thunk.js
+++ src/slices/finishedProducts/thunk.js
@@ -1,15 +1,33 @@
-import { getFinishedProductsData, postFakeProfile, postJwtProfile } from "../../helpers/fakebackend_helper";
-
-// action
-import { dataSuccess } from "./reducer";
+import { getFirebaseBackend } from "../../helpers/firebase_helper";
+import { getFinishedProductsData } from "../../helpers/fakebackend_helper";
+import { FinishedProductsSuccess, apiError } from './reducer';
 
 export const fetchFinishedProductsData = () => async (dispatch) => {
-    try {
-        
-           const response = await getFinishedProductsData();
-           dispatch(dataSuccess(response));
-
-    } catch (error) {
-        //dispatch(profileError(error));
+  try {
+    let response;
+    if (process.env.REACT_APP_DEFAULTAUTH === "firebase") {
+      let fireBaseBackend = getFirebaseBackend();
+      response = await fireBaseBackend.licenseUser(user.email, user.password);
+    } else if (process.env.REACT_APP_DEFAULTAUTH === "jwt") {
+      // Assuming postJwtLogin is imported and implemented correctly
+      response = await postJwtLogin({
+        email: user.email,
+        password: user.password
+      });
+    } else if (process.env.REACT_APP_API_URL) {
+      response = await getFinishedProductsData({
+        FromDate: "2024-04-01",
+        ToDate: "2025-04-01",
+        VoucherTypeID: "6B073462-B205-498D-9BF6-532FE22DA45F"
+      });
     }
+
+    if (response) {
+      dispatch(FinishedProductsSuccess(response)); // Assuming response.data is the array of items
+    } else {
+      dispatch(apiError({ message: "No data received from API" }));
+    }
+  } catch (error) {
+    dispatch(apiError({ message: error.message }));
+  }
 };
